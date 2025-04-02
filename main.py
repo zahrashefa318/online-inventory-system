d6761ebf3b6dd008a45818503ec7bf7bb6b89afe
Zahra <zahra.shefa.1990@gmail.com>
2025-04-02 13:36:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def home():
     return render_template('base.html')
 
 
-
+port = os.environ.get('PORT', 10000)
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=port) 
